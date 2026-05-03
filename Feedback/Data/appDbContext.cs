bb9cd17cb18597aa4d7ebb8576b8954c9f38c45e
MagedMedhat <magedmedhat.exe@gmail.com>
2026-05-03 04:09:34 +0300

    Remove commented SQL Server configuration
    Removed commented-out SQL Server configuration from appDbContext.

--- Feedback/Data/appDbContext.cs
+++ Feedback/Data/appDbContext.cs
@@ -13,9 +13,6 @@ namespace Feedback.Data
         public DbSet<Review> Reviews { get; set; }
         public DbSet<Comment> Comments { get; set; }
 
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    optionsBuilder.UseSqlServer("Server=db50577.databaseasp.net; Database=db50577; User Id=db50577; Password=Z#j7?6Lgd+2H; Encrypt=False; MultipleActiveResultSets=True;");
-        //}
+        
     }
 }
